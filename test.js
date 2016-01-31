42529d9f442cc37a6485a03f761e08e77693877a
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-31 18:35:08 +0100

    test(package): add api test suite

---
+++ test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('assert');
+const passord = require('.');
+
+describe('passord()', () => {
+  it('returns random password', () => {
+    assert(/^[a-zæøåA-ZÆØÅ]+[0-9]{1,3}$/.test(passord()));
+  });
+});
+
+describe('codeword()', () => {
+  it('returns random codeword', () => {
+    assert(/^[A-ZÆØÅ]+ [A-ZÆØÅ]+$/.test(passord.codeword()));
+  });
+});
+
+describe('combinations()', () => {
+  it('returns number of combinations', () => {
+    assert.equal(passord.combinations(), 139812000);
+  });
+});
+
+describe('dictionary', () => {
+  it('allows custom dictionary', () => {
+    passord.dictionary.adjectiv = ['stor', 'listen'];
+    passord.dictionary.noun = ['fotball', 'tennisball', 'golfball'];
+
+    assert.equal(passord.combinations(), 24000);
+  });
+});
